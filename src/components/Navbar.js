57fbd6be44e7572f8810443054b5f89fcaa32d53
Abel Seifu <abel.workmail@gmail.com>
2023-02-07 12:38:57 +0300

    Create a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/logo.png";
+
+const Navbar = () => (
+  <header>
+    <div>
+      <img src={logo} alt={logo} width="50" height="50" />
+    </div>
+  </header>
+);
+
+export default Navbar;
