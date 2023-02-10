f0b0b641b8a3650fa9df0c1740fe2f5a7aae338a
Abel Seifu <abel.workmail@gmail.com>
2023-02-11 01:14:59 +0300

    Linter issues

--- src/tests/missions.test.js
+++ src/tests/missions.test.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import MissionList from '../components/missions/missionList';
 import store from '../redux/configureStore';
+import MissionList from '../components/missions/missionList';
 import { fetchMissions, joinMission, leavingMission } from '../redux/missions/missionReducer';
 
 describe('display the mission in the page', () => {
